b041c7a26106631a18e1342debdc383ac3be640c
Grace <112779767+grachimus@users.noreply.github.com>
2023-01-21 16:36:14 -0600

    Fixed seed data. Inserted sample images. Created artist cards.

--- seeds/genreSeed.js
+++ seeds/genreSeed.js
@@ -4,58 +4,72 @@ const genreData = [
   {
     name: "R&B",
     image: "",
+    banner:""
   },
   {
     name: "Pop",
     image: "",
+    banner:""
   },
   {
     name: "Hip-Hop",
     image: "",
+    banner:""
   },
   {
     name: "Latin",
     image: "",
+    banner:""
   },
   {
     name: "Electronic",
     image: "",
+    banner:""
   },
   {
     name: "DJ",
     image: "",
+    banner:""
   },
   {
     name: "Dance/Alternative",
     image: "",
+    banner:""
   },
   {
     name: "Rock",
     image: "",
+    banner:""
   },
   {
     name: "Indie",
     image: "",
+    banner:""
   },
   {
     name: "Soul/Funk",
     image: "",
+    banner:""
   },
   {
     name: "Folk/Acoustic",
     image: "",
+    banner:""
   },
   {
     name: "Metal",
     image: "",
+    banner:""
   },
   {
     name: "Blues",
     image: "",
+    banner:""
   },
   {
     name: "Country",
     image: "",
+    banner:""
   },
 ];
 

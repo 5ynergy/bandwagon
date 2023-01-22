53ab393f9e5c47ef40a7624a26f469afeded870b
Grace <112779767+grachimus@users.noreply.github.com>
2023-01-21 22:32:03 -0600

    added Loader to Event creation

--- public/js/loginLoader.js
+++
@@ -1,7 +0,0 @@
-const segment = document.getElementById("login-segment")
-const loginButton = document.getElementById("login-button");
-
-loginButton.addEventListener("click", (e)=> {
-    $('.segment').dimmer('show');
-})
-

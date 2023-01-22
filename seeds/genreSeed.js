73c4f135a0c2c95c519d357b5a694f336d1e4f2a
jtweedle1 <jazmin.tweedle@gmail.com>
2023-01-21 22:47:58 -0600

    got gradient background for banners to work

--- seeds/genreSeed.js
+++ seeds/genreSeed.js
@@ -4,37 +4,37 @@ const genreData = [
   {
     name: "R&B",
     image: "",
-    banner:""
+    banner:"https://images.pexels.com/photos/6985132/pexels-photo-6985132.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     name: "Pop",
     image: "",
-    banner:""
+    banner:"https://images.pexels.com/photos/7135073/pexels-photo-7135073.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     name: "Hip-Hop",
     image: "",
-    banner:""
+    banner:"https://images.pexels.com/photos/6985001/pexels-photo-6985001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     name: "Latin",
     image: "",
-    banner:""
+    banner:"https://images.pexels.com/photos/7130465/pexels-photo-7130465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     name: "Electronic",
     image: "",
-    banner:""
+    banner:"https://images.pexels.com/photos/6985048/pexels-photo-6985048.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     name: "DJ",
     image: "",
-    banner:""
+    banner:"https://images.pexels.com/photos/7135028/pexels-photo-7135028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     name: "Dance/Alternative",
     image: "",
-    banner:""
+    banner:"https://images.pexels.com/photos/7135074/pexels-photo-7135074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     name: "Rock",

5fbb7fb5189e06869dee0d14ffc9dacfad3d66b8
Grace <112779767+grachimus@users.noreply.github.com>
2023-01-23 17:14:10 -0600

    deleted unecessary images, created social media segment

--- seeds/seed.js
+++ seeds/seed.js
@@ -6,8 +6,6 @@ const seedGenres = require('./genreSeed');
 const seedUsers = require('./userSeed');
 const seedEvents = require('./eventSeed')
 
-//Jazmin's code
-
 const seedData = async () => {
   await sequelize.sync({ force: true });
   await seedGenres();
@@ -21,26 +19,3 @@ const seedData = async () => {
 };
 
 seedData();
-
-// //Grace's code
-// const genreData = require('./genreData.json');
-// const userData = require('./userData.json');
-// const eventData = require('./eventData.json')
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   const users = await User.bulkCreate(userData);
-
-//   // const users = await User.bulkCreate(userData, {
-//   //   individualHooks: true,
-  //   returning: true,
-  // });
-
-  // const events = await Event.bulkCreate(eventData);
-  // const genre = await Genre.bulkCreate(genreData);
-
-//   process.exit(0);
-// };
-
-// seedDatabase();
